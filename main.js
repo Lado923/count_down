485f969dc911a98d6aae7ca805c9295ff85773f1
Lado923 <slava923@gmail.com>
2022-12-13 10:37:31 +0300

    feat: add basic code to files

---
+++ main.js
@@ -0,0 +1,44 @@
+const semicircles = document.querySelectorAll('.semicircle');
+
+// const dd = 0;
+const hr = 0;
+const min = 0;
+const sec = 10;
+
+// const days = dd * 86400000;
+const hours = hr * 3600000;
+const minutes = min * 60000;
+const seconds = sec * 1000;
+const setTime = hours + minutes + seconds;
+// const setTime = days + hours + minutes + seconds;
+// const startTime = new Date('December 5, 2022 19:00:00');
+// const futureTime = new Date('January 16, 2023 05:35:00');
+const startTime = Date.now();
+const futureTime = startTime + setTime;
+
+
+
+const timerLoop = setInterval(countDownTimer);
+countDownTimer();
+
+function countDownTimer() {
+const currentTime = Date.now();
+const remainingTime = futureTime - currentTime;
+
+
+const angle = (remainingTime / setTime) * 360;
+
+if(angle > 190) {
+    semicircles[2].style.display = 'none';
+    semicircles[o].style.transform = 'rotate(180deg)';
+    semicircles[1].style.transform = 'rotate(${angle}deg)';
+
+} else {
+    semicircles[2].style.display = 'block';
+    semicircles[o].style.transform = 'rotate(${angle}deg)';
+    semicircles[1].style.transform = 'rotate(${angle}deg)';
+
+}
+
+}
+
